b72a437af465f00ce1c1416b7f0c50b39e8f13ef
aburlakov <alexanderburlakov98@gmail.com>
2016-05-24 22:25:55 +0300

    test guthub

---
+++ problem1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal zelcena = decimal.Parse(Console.ReadLine());
+            decimal plodcena = decimal.Parse(Console.ReadLine());
+            int zel = int.Parse(Console.ReadLine());
+            int plod = int.Parse(Console.ReadLine());
+
+            decimal sum = zelcena*zel + plod*plodcena;
+            decimal sum1 = sum / 1.94m;
+            Console.WriteLine(sum1);
+            Console.ReadLine();
+        }
+    }
+}
